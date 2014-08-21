d747c322472f671cef9449ad5f38ce58a51ce947
Deirdre Connolly <durumcrustulum@gmail.com>
2014-08-21 11:19:17 -0400

    Updated to only observe the new items container

--- main.js
+++ main.js
@@ -15,26 +15,21 @@ var checkForNewTweets = function() {
 };
 
 // Set up Mutation Observers for 'new tweets' dom insert.
-var targetNodes         = $(".stream-container");
-var MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
-var myObserver          = new MutationObserver (mutationHandler);
-var obsConfig           = { childList: true, characterData: true, attributes: true, subtree: true };
+var targetNodes = $(".stream-container .js-new-items-bar-container");
+var Observer = window.MutationObserver || window.WebKitMutationObserver;
+var myObserver = new Observer(mutationHandler);
+var obsConfig = { childList: true, characterData: true, attributes: true, subtree: true };
 
 //--- Add a target node to the observer. Can only add one node at a time.
-targetNodes.each ( function () {
-    myObserver.observe (this, obsConfig);
-} );
+targetNodes.each (function () {
+  myObserver.observe (this, obsConfig);
+});
 
 // Watch for possible 'new tweets' element insertion, call loader.
 function mutationHandler (mutationRecords) {
-    mutationRecords.forEach(function (mutation) {
-        if (mutation.addedNodes.length > 0) {
-            for (var n in mutation.addedNodes) {
-                var node = mutation.addedNodes[n];
-                if (node.className == 'stream-item') {
-                    checkForNewTweets();
-                }
-            }
-        }
-    });
+  mutationRecords.forEach(function (mutation) {
+    if (mutation.addedNodes.length > 0) {
+      checkForNewTweets();
+    }
+  });
 }
